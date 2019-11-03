335221dedaff481bb8b347a24db5bb24a34e91a3
Justin W <s0181433@student.rockvalleycollege.edu>
2019-11-03 15:51:20 -0600

    Initial Commit

---
+++ MyMovies10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies10
+//
+//  Created by Justin Williams on 11/3/19.
+//  Copyright © 2019 Justin Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
